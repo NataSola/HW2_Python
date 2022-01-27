b9d450913b20db1e493b5a165d4a6058292cb8b6
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-01-27 23:58:30 +0300

    rewrite code

--- 01_Suare_or_not.py
+++ 01_Suare_or_not.py
@@ -3,13 +3,7 @@
 a = int(input('Введите число: '))
 b = int(input('Введите число: '))
 
-def Check_square(a, b):
-    if a**2 == b:
-        return f'Число {b} является квадратом числа {a}'
-    elif b**2 == a:
-        return f'Число {a} является квадратом числа {b}'
-    else:
-        return 'Ни одного из чисел не является квадратом другого'
+def chek_sqr(a, b):
+    return a**2 == b or b**2 == a
 
-cheking_result = Check_square(a, b)
-print(cheking_result)
+print (chek_sqr(a,b))
