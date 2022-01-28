8f32e80bd486c8c1688721f060d0cf2102ca5e5e
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-01-29 00:47:38 +0300

    add task 12

---
+++ 12_Index-value.py
@@ -0,0 +1,12 @@
+# 12. Для натурального N создать словарь индекс-значение,
+# состоящий из элементов последовательности 3k + 1.
+
+from random import randint
+
+def get_dict(n):
+    return {x: 3 * x + 1 for x in range(n)}
+
+n = randint(5, 20)
+
+print(n)
+print (get_dict(n))
