458f46042c6944f1de2511399fcfc58c7ae53264
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-01-28 23:11:24 +0300

    Add task 9

---
+++ 09_Coords_diapasone.py
@@ -0,0 +1,12 @@
+# 9. Указав номер четверти прямоугольной системы координат, 
+# показать допустимые значения координат для точек этой четверти
+
+from random import randint
+
+quarter = randint(1, 4)
+diapasones = ['(x > 0, y > 0)', '(x < 0, y > 0)', '(x < 0, y < 0)', '(x > 0, y < 0)']
+
+def show_diapasone (quart, diapasones):
+    return diapasones[quarter-1]
+
+print (f'{quarter}: {diapasones[quarter-1]}')
