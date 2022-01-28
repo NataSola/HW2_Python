12c65ff21a8237098ab4d99db78c593f797fd63a
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-01-28 23:15:42 +0300

    add task 10

---
+++ 10_Point_distance.py
@@ -0,0 +1,20 @@
+# 10. Найти расстояние между двумя точками пространства
+
+from random import randint
+import math
+
+def Get_coordinates(num_plan, left, right):     # координаты точек
+    return [randint(left, right) for i in range(num_plan)]
+
+def Find_dist(a, b):    # расстояние между точками
+    return round(math.sqrt(sum((x-y)**2 for x,y in zip(a, b))), 3)
+
+num_plan = 3    # количество осей координат
+left = -10
+right = 10
+
+point_A = Get_coordinates(num_plan, left, right)
+point_B = Get_coordinates(num_plan,left, right)
+
+print(f'A {point_A}, B {point_B}')
+print (f'Расстояние между точками: {Find_dist(point_A, point_B)}')
