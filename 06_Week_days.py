aaf0ffd64d873ea95a0fd5506023081efc721a2c
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-01-28 22:26:14 +0300

    add solving task

--- 06_Week_days.py
+++ 06_Week_days.py
@@ -1,16 +1,20 @@
-# 6. Дано число обозначающее день недели. 
+# 6. Дано число обозначающее день недели.
 # Вывести его название и указать является ли он выходным.
 
-week_days = ['Воскресенье', 'Суббота', 'Пятница', 'Четверг', 'Среда', 'Вторник', 'Понедельник', [' выходной день', ' выходной день']]
+from calendar import weekday
+from datetime import datetime
+
+
+week_days = ('Понедельник', 'Вторник', 'Среда', 'Четверг',
+             'Пятница', 'Суббота', 'Воскресенье')
 number = int(input('Введите номер дня недели: '))
 
+
 def Find_day(list, num):
-    if 0 < num < 8:
-        return week_days[i][j]
-    else:
-        return 'Дня недели с таким номером не существует'
+    if 0 < num < 6:
+        return week_days[num-1]
+    elif 5 < num < 7:
+        return f'{week_days[num-1]}, выходной день'
 
 day_name = Find_day(week_days, number)
-print (day_name)
-
-
+print(day_name)
