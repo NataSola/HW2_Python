aa3f8fca49f2390227972e39ab050d733d1abee5
NataSola <nsol2009@yandex.ru>
2022-01-28 14:35:41 +0300

    Rename method

--- 04_First_frctional_digit.py
+++ 04_First_frctional_digit.py
@@ -2,9 +2,9 @@
 
 from random import uniform
 
-def get_frst_fractional (n):
-    return int(n / 0.1 % 10)
+def get_frst_fract_digit (n):
+    return int(n * 10 % 10)
 
 n = uniform(10,1000)
 print(n)
-print(get_frst_fractional(n))
+print(get_frst_fract_digit(n))
