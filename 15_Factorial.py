ce6e2d2b63314815be309301be5b684d8fee0780
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-01-29 20:34:59 +0300

    Add task 15

---
+++ 15_Factorial.py
@@ -0,0 +1,13 @@
+# 15. Написать программу получающую набор произведений чисел от 1 до N.
+
+N = int(input('Введите число: '))
+
+def get_factorial_list(n):
+    fact = 1
+    facts = []
+    for i in range(1, n+1):
+        fact *= i
+        facts.append(fact)
+    return facts
+
+print(get_factorial_list(N))
