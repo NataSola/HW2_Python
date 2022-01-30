88d23725c69d2c38b329f953fef3c954ff3c6f2d
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-01-30 22:26:23 +0300

    fixed bugs

--- 12_Index-value.py
+++ 12_Index-value.py
@@ -4,9 +4,9 @@
 from random import randint
 
 def get_dict(n):
-    return {x: 3 * x + 1 for x in range(n)}
+    return {x: 3 * x + 1 for x in range(1, n+1)}
 
 n = randint(5, 20)
 
 print(n)
-print (get_dict(n))
+print(get_dict(n))
