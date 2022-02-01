cfa78e059c9df8f7147a7f3b1562579c0d36d8db
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-02-01 11:09:37 +0300

    add tsk 31

--- 31_Prime_factors.py
+++ 31_Prime_factors.py
@@ -2,7 +2,6 @@
 
 import math
 
-
 def is_prime(n):
     primes = [2]
     for num in range(3, n + 1, 2):
@@ -10,6 +9,24 @@ def is_prime(n):
             primes.append(num)
     return primes
 
+def get_prime_factors(n, primes):
+    fact = []
+    while n % 2 == 0:
+        n = n / 2
+        fact.append(2)
+    for i in primes:
+        while n % i == 0:
+            n = n / i
+            fact.append(i)
+    return fact
 
-primes = is_prime(100)
-print(primes)
+n = int(input('Введите число: '))
+
+primes = is_prime(n)
+factors = get_prime_factors(n, primes)
+print(factors)
+
+def testing(n, factors):
+    return math.prod(factors) == n       
+
+print(testing(n, factors))
