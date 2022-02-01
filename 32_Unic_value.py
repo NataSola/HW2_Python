8f7e136e614f5508bec8d036a2518e6b0cd1ece1
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-02-01 11:25:49 +0300

    add task 32

--- 32_Unic_value.py
+++ 32_Unic_value.py
@@ -7,17 +7,12 @@ def create_list(size, m, n):
     return [randint(m, n) for i in range(size)]
 
 def get_unic_value(list):
-    # list2 = []
-    return set(list)
-    # [i for i in range[list] if not in rage(list2)]
+    return [i for i in set(list)]
 
-size = 25
-m = 10
-n = 100
+size = 10
+m = 1
+n = 10
 
 origin = create_list(size, m, n)
 print(origin)
-
-print(get_unic_value)
-
-
+print(get_unic_value(origin))
--- gg.py
+++ gg.py
@@ -1,6 +1,13 @@
 import math
-print (2)
-for num in range(3,101,2):
-    if all(num%i!=0 for i in range(2,int(math.sqrt(num))+1)):
-       print (num)
+numbers = [1, 2, 2, 3, 3, 4, 5]
 
+def get_unique_numbers(numbers):
+    list_of_unique_numbers = []
+    unique_numbers = set(numbers)
+
+    for number in unique_numbers:
+        list_of_unique_numbers.append(number)
+
+    return list_of_unique_numbers
+
+print(get_unique_numbers(numbers))
