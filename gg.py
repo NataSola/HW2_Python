0279f51352e3e35db15a0d3466d51597d60b2122
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-02-02 01:44:21 +0300

    change in drafts

--- gg.py
+++ gg.py
@@ -1,13 +1,70 @@
 import math
-numbers = [1, 2, 2, 3, 3, 4, 5]
+import decimal
 
-def get_unique_numbers(numbers):
-    list_of_unique_numbers = []
-    unique_numbers = set(numbers)
 
-    for number in unique_numbers:
-        list_of_unique_numbers.append(number)
+from math import pi
+from decimal import Decimal
 
-    return list_of_unique_numbers
 
-print(get_unique_numbers(numbers))
+n = pi
+print(n)
+print(format(n,'.100'))
+print(format(Decimal.from_float(n), '.100f')) 
+
+
+
+
+
+# d = '3' /
+# dd = '.{d}f'
+# print(dd)
+# print ('Простой вывод пи на печать')
+# print(10**(-10))
+
+# print ("Вывод пи на печать (200 знаков) с использованием format(n,'.200')")
+# print(format(pi,'.200'))
+# print()
+
+# print ('Вывод на печать (100 знаков) пи с использованием Decimal')
+# print(format(Decimal.from_float(pi), '.100f')) 
+# print()
+
+# print ('Простой вывод на печать пи * 10^(-100)')
+# print(n)
+# print()
+
+# print ('Вывод на печать (400 знаков): пи * 10^(-100) с использованием Decimal')
+# print(format(Decimal.from_float(n), '.400f')) 
+# print()
+
+
+# print(round(pi, 20))
+# print(format(n,'.400f'))
+
+
+
+
+
+
+# def task31 (n):
+#     i = 2
+#     array = []
+#     while n != 1: 
+#         if n % i == 0:
+#             array.append(i) #3
+#             n = n / i
+#             i = 2
+#         else: 
+#             i += 1
+#     return (array)
+
+# n = 549
+# ff = task31 (n)
+# print (ff)
+
+# def testing(n, ff):
+#     return math.prod(ff) == n       
+
+# print(testing(n, ff))
+
+# # n_str = str(n).replace('.','') 
---
+++ ss.py
@@ -0,0 +1,75 @@
+from math import pi
+
+def sqrt(n, one):
+     """
+     Return the square root of n as a fixed point number with the one
+     passed in.  It uses a second order Newton-Raphson convergence.  This
+     doubles the number of significant figures on each iteration.
+     """
+     # Use floating point arithmetic to make an initial guess
+     floating_point_precision = 10**16
+     n_float = float((n * floating_point_precision) // one) / floating_point_precision
+     x = (int(floating_point_precision * math.sqrt(n_float)) * one) // floating_point_precision
+     n_one = n * one
+     while 1:
+         x_old = x
+         x = (x + n_one // x) // 2
+         if x == x_old:
+             break
+     return x
+
+def pi_chudnovsky_bs(digits):
+
+    """
+    Compute int(pi * 10**digits)
+
+    This is done using Chudnovsky's series with binary splitting
+    """
+    C = 640320
+    C3_OVER_24 = C**3 // 24
+    def bs(a, b):
+        """
+        Computes the terms for binary splitting the Chudnovsky infinite series
+
+        a(a) = +/- (13591409 + 545140134*a)
+        p(a) = (6*a-5)*(2*a-1)*(6*a-1)
+        b(a) = 1
+        q(a) = a*a*a*C3_OVER_24
+
+        returns P(a,b), Q(a,b) and T(a,b)
+        """
+        if b - a == 1:
+            # Directly compute P(a,a+1), Q(a,a+1) and T(a,a+1)
+            if a == 0:
+                Pab = Qab = 1
+            else:
+                Pab = (6*a-5)*(2*a-1)*(6*a-1)
+                Qab = a*a*a*C3_OVER_24
+            Tab = Pab * (13591409 + 545140134*a) # a(a) * p(a)
+            if a & 1:
+                Tab = -Tab
+        else:
+            # Recursively compute P(a,b), Q(a,b) and T(a,b)
+            # m is the midpoint of a and b
+            m = (a + b) // 2
+            # Recursively calculate P(a,m), Q(a,m) and T(a,m)
+            Pam, Qam, Tam = bs(a, m)
+            # Recursively calculate P(m,b), Q(m,b) and T(m,b)
+            Pmb, Qmb, Tmb = bs(m, b)
+            # Now combine
+            Pab = Pam * Pmb
+            Qab = Qam * Qmb
+            Tab = Qmb * Tam + Pam * Tmb
+        return Pab, Qab, Tab
+    # how many terms to compute
+    DIGITS_PER_TERM = math.log10(C3_OVER_24/6/2/6)
+    N = int(digits/DIGITS_PER_TERM + 1)
+    # Calclate P(0,N) and Q(0,N)
+    P, Q, T = bs(0, N)
+    one = 10**digits
+    sqrtC = sqrt(10005*one, one)
+    return (Q*426880*sqrtC) // T
+
+
+print(pi_chudnovsky_bs(100))
+print(pi)
