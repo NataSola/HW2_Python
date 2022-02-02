09c4aa0414e9e0bca9bc6790a2195571c0c533ce
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-02-03 00:25:47 +0300

    change add type tuple (str.7)

--- 10_Point_distance.py
+++ 10_Point_distance.py
@@ -4,10 +4,10 @@ from random import randint
 import math
 
 def Get_coordinates(num_plan, left, right):     # координаты точек
-    return [randint(left, right) for i in range(num_plan)]
+    return tuple([randint(left, right) for i in range(num_plan)])
 
 def Find_dist(a, b):    # расстояние между точками
-    return round(math.sqrt(sum((x-y)**2 for x,y in zip(a, b))), 3)
+    return round(math.sqrt(sum((x - y)**2 for x, y in zip(a, b))), 3)
 
 num_plan = 3    # количество осей координат
 left = -10
