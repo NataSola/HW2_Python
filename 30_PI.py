f14a318c6ad37ed56d1507c4d3f786c6684d9122
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-02-04 01:02:47 +0300

    task 30 solved

--- 30_PI.py
+++ 30_PI.py
@@ -1,32 +1,14 @@
 # 30. Вычислить число pi c заданной точностью d
 # 	  Пример: при d = 0.001,  pi = 3.141. 10^(-1) <= d10 <= 10^(-10)
 
-from cmath import acos
+import math
 from math import pi
-from decimal import Decimal
-from math import acos
 
-# import sympy
-print()
 n = pi
-# print(f'pi:      {n}')
-# print(f'355/113: {355/113}')
-print(format(pi,'.100'))
-# print(format(Decimal.from_float(n), '.100f')) 
-print()
+print(n)
 
+n=20000000
 
-# import math 
-# def CalculatePi(roundVal):
-# 		somepi = round(math.pi,roundVal);
-# 		pi = str(somepi)
-# 		someList = list(pi)
-# 		return somepi;
-# roundTo = input('Enter the number of digits you want after the decimal for Pi: ')
-# try:
-# 	roundint = int(roundTo);
-# 	print(CalculatePi(roundint));
-# except:
-# 	print("You did not enter an integer");
+mypi = 4 * (sum(1/x for x in range (1, n + 1, 4)) + sum(-1/x for x in range (3, n + 1, 4)))
 
-
+print(format(mypi,'.100')) 
