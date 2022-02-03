0c65f361c11544d930376190ffdf78e005373781
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-02-03 15:45:11 +0300

    solved task 33

--- 33_Polynomial.py
+++ 33_Polynomial.py
@@ -1,45 +1,40 @@
 # 33. Задана натуральная степень k. Сформировать случайным образом список коэффициентов 
 # (значения от 0 до 100) многочлена и записать в файл многочлен степени k.  2*х^2 + 5*х + 33 = 0
 
+from dataclasses import replace
 from random import randint, choice
 
 
-n = 2 #int(input('k= '))
+pp = '2*х^2 + 5*х + 33 = 0'
+pol = ['2','*x^','2',' + ', '5', '*x + ','33',' = 0']
+
+k = 3
+
+ratios = [str(randint(0, 101)) for x in range(k)]
+indexes = [0, 4, 6, 2]
+
+def change_poly(pol, indexes, ratios, k):
+    ratios.append(str(k))
+    for indexes, replacement in zip(indexes, ratios):
+        pol[indexes] = replacement
+    return "".join(pol)
+
+pol_new = change_poly(pol, indexes, ratios, k)
+print(pol_new)
+
+# В одну строку
+
+def change_polynomial(pol, k, rat, ind):
+    new_pol = []
+    rat.append(str(k))
+    new_pol = [ratios[ind.index(i)] if i in ind else pol[i] for i in range(len(pol))]
+    return "".join(new_pol)
+
+new_pol = change_polynomial(pol, k, ratios, indexes)
+print(new_pol)
+
+with open('33_Polynomial.txt', 'w') as data:
+    data.write(pol_new)
 
 
-k_list = [randint(0, 101) for x in range(-10, 11)]
-k = choice(k_list)
-# pol = choice(k_list) * x ** n + choice(k_list) * x + choice(k_list)
-# pol_member = (k + '* x ** ' + n) 
-
-def get_pol(lst, n):
-    pol = str()
-    for i in range (n+1):
-        for j in range (3):
-            pol.join('jjj')
-            pol.join(str(choice(lst)))
-            pol.join(' *  x **')
-            pol.join(str(i))
-            # pol.join(' + ')
-            # pol.join(choice(lst))
-            # pol.join(' * x ')
-            # pol.join(choice(lst))
-    return pol
-
-
-    # return str (map (lambda x: x ))
-
-
-
-# print(k_list)
-# print(k)
-pol_member = []
-pol_member.append(str(choice(k_list)))
-pol_member.append('* x **')
-pol_member.append(str(n))
-polm_str = " ".join(pol_member)
-
-# pol = get_pol(k_list, n)
-# print(type(pol))
-print(pol_member)
-print(polm_str)
+# print(list(enumerate(pol)))
