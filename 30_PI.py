dad3930b7d4f4fc51f04329220a0e779a9b6bc2a
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-02-04 10:48:52 +0300

    add the Bailey — Boruain — Plaff formula

--- 30_PI.py
+++ 30_PI.py
@@ -7,8 +7,18 @@ from math import pi
 n = pi
 print(n)
 
-n=20000000
+# Формула Бэйли — Боруэйна — Плаффа
 
-mypi = 4 * (sum(1/x for x in range (1, n + 1, 4)) + sum(-1/x for x in range (3, n + 1, 4)))
+n = 100
+my_pi = sum(1/16**x*(4/(8*x + 1) - 2/(8*x + 4) - 1/(8*x + 5) - 1/(8*x + 6)) for x in range(n))
 
-print(format(mypi,'.100')) 
+print(my_pi)
+
+# Ряд Лейбница
+
+# n = 20000000
+
+# mypi = 4 * (sum(1/x for x in range(1, n + 1, 4)) +
+#             sum(-1/x for x in range(3, n + 1, 4)))
+
+# print(format(mypi, '.100'))
