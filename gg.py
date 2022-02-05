44c0c20caf5c6f789bb96dede05c8b145617a053
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-02-05 23:06:34 +0300

    drafts

--- gg.py
+++ gg.py
@@ -1,70 +1,11 @@
 import math
-import decimal
-
-
 from math import pi
-from decimal import Decimal
-
 
 n = pi
 print(n)
-print(format(n,'.100'))
-print(format(Decimal.from_float(n), '.100f')) 
 
+n=20000000
 
+mypi =4* (sum(1/x for x in range (1, n + 1,4) ) + sum(-1/x for x in range (3, n + 1, 4)))
 
-
-
-# d = '3' /
-# dd = '.{d}f'
-# print(dd)
-# print ('Простой вывод пи на печать')
-# print(10**(-10))
-
-# print ("Вывод пи на печать (200 знаков) с использованием format(n,'.200')")
-# print(format(pi,'.200'))
-# print()
-
-# print ('Вывод на печать (100 знаков) пи с использованием Decimal')
-# print(format(Decimal.from_float(pi), '.100f')) 
-# print()
-
-# print ('Простой вывод на печать пи * 10^(-100)')
-# print(n)
-# print()
-
-# print ('Вывод на печать (400 знаков): пи * 10^(-100) с использованием Decimal')
-# print(format(Decimal.from_float(n), '.400f')) 
-# print()
-
-
-# print(round(pi, 20))
-# print(format(n,'.400f'))
-
-
-
-
-
-
-# def task31 (n):
-#     i = 2
-#     array = []
-#     while n != 1: 
-#         if n % i == 0:
-#             array.append(i) #3
-#             n = n / i
-#             i = 2
-#         else: 
-#             i += 1
-#     return (array)
-
-# n = 549
-# ff = task31 (n)
-# print (ff)
-
-# def testing(n, ff):
-#     return math.prod(ff) == n       
-
-# print(testing(n, ff))
-
-# # n_str = str(n).replace('.','') 
+print(format(mypi,'.100'))
