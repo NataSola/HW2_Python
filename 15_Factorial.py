ecad2829ec98630e890f52917fe074d41597230b
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-02-07 00:55:26 +0300

    optimise

--- 15_Factorial.py
+++ 15_Factorial.py
@@ -1,16 +1,18 @@
 # 15. Написать программу получающую набор произведений чисел от 1 до N.
 
+from itertools import accumulate
+import operator
+
 N = int(input('Введите число: '))
 
-def get_factorial_list(n):
-    fact = 1
-    facts = []
-    for i in range(1, n+1):
-        fact *= i
-        facts.append(fact)
-    return facts
 
-print(get_factorial_list(N))
+def get_prods(N):
+    return list(accumulate([x for x in range(1, N + 1)], operator.mul))
+
+print(get_prods(N))
+
+
+# Предыдущий вариант решения
 
 # def get_factorial_list(n):
 #     fact = 1
@@ -18,5 +20,6 @@ print(get_factorial_list(N))
 #     for i in range(1, n+1):
 #         fact *= i
 #         facts.append(fact)
-#     return [x * i for i in range (1, n + 1)]
+#     return facts
 
+# print(get_factorial_list(N))
