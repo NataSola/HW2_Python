93d501a6e2bd7200f8566344a2a848017ff26d66
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-02-07 02:09:42 +0300

    add task 38

---
+++ 38_Delete_words.py
@@ -0,0 +1,12 @@
+# 38. Напишите программу, удаляющую из 
+# текста все слова содержащие "абв".
+
+my_text = 'Напишите абв напиабв програбвмму программу, удаляющую из \
+    этого абв текста все вабвс слова, содерабващие содержащие "абв"'
+
+def del_some_words(my_text):
+    my_text = list(filter(lambda x: 'абв' not in x, my_text.split()))
+    return " ".join(my_text)
+
+my_text = del_some_words(my_text)
+print(my_text)
