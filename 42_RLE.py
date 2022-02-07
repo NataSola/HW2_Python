8fd13467abcd036e83fa37476ca13e0355c53ba1
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-02-08 00:34:16 +0300

    Add task 42

---
+++ 42_RLE.py
@@ -0,0 +1,41 @@
+# 42. Реализовать RLE алгоритм. реализовать модуль сжатия и восстановления данных.
+    # - входные и выходные данные хранятся в отдельных файлах
+
+
+with open('42_RLE1_decoded.txt', 'r') as data:
+    my_text = data.read()
+
+def encode_rle(ss):
+    str_code = ''
+    prev_char = ''
+    count = 1
+    for char in ss:
+        if char != prev_char:
+            if prev_char:
+                str_code += str(count) + prev_char
+            count = 1
+            prev_char = char
+        else:
+            count += 1
+    return str_code
+
+            
+str_code = encode_rle(my_text)
+print(str_code)
+
+with open('42_RLE2_encoded.txt', 'r') as data:
+    my_text2 = data.read()
+
+def decoding_rle(ss:str):
+    count = ''
+    str_decode = ''
+    for char in ss:
+        if char.isdigit():
+            count += char 
+        else:
+            str_decode += char * int(count)
+            count = ''
+    return str_decode
+
+str_decode = decoding_rle(my_text2)
+print(str_decode)
