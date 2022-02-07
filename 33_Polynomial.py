2bba26007c8a8d5227b4508d6670cb481cc4d2aa
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-02-07 23:38:43 +0300

    optimise

--- 33_Polynomial.py
+++ 33_Polynomial.py
@@ -5,37 +5,43 @@
 from random import randint
 import itertools
 
-k = 5
+k = randint(2, 5)
 
 ratios = [randint(0, 10) for i in range (k + 1)]
 while ratios[0] == 0:
     ratios[0] = randint(1, 10) 
 
-# ratios = [5, 0, 1, 8, 0]
-
 def get_polinom(k, rat):
     prods = ['*x'] * k  
     degrees = ['^'] * (k - 1)
-    n = [n for n in range(k, 1, -1)]    # список степеней в обратном порядке от большего до 2
-    additions = [' + '] * k
-    rat = list(map(str, rat))       # преобразование чисел (список коэффициентов) в строковые значения
-    pol = list(itertools.zip_longest(rat, prods, degrees, n, additions, fillvalue=''))  # собираем члены многочлена в виде кортежей
-    pol = [x for x in pol if x[0] != '0']      # оставляем элементы (кортежи), коэффициент != 0
-    pol = [list(x) for x in pol]        # преобразуем кортежи в списки  
+    n = [n for n in range(k, 1, -1)]    # список степеней в обратном порядке от большей до 2            
+    pol = list(itertools.zip_longest(map(str, rat), prods, degrees, n,  fillvalue=''))  # собираем члены многочлена в виде кортежей
+    pol = [list(x) for x in pol if x[0] != '0']      
     for x in pol[0:-1]:
         if x[0] == '1': del x[0]
         if x[0] == '*x': x[0] = 'x'
-    if pol[-1][-1] == ' + ': del pol[-1][-1]
+        x.append(' + ')
     pol.append([' = 0'])
-    pol = list(itertools.chain(*pol))   # преобразование списка списков в список
-    return "".join(map(str, pol))   # преобразование списка в строку
+    pol = list(itertools.chain(*pol))  
+    return "".join(map(str, pol))   
 
-
-# ratios = get_ratios(k)
-polynom = get_polinom(k,ratios)
-print(polynom)
-
-print(ratios)
+polynom1 = get_polinom(k,ratios)
+print(polynom1)
 
 with open('33_Polynomial.txt', 'w') as data:
-    data.write(polynom)
+    data.write(polynom1)
+
+
+# Второй многочлен для следующей задачи:
+
+k = randint(2, 5)
+
+ratios = [randint(0, 10) for i in range (k + 1)]
+while ratios[0] == 0:
+    ratios[0] = randint(1, 10) 
+
+polynom2 = get_polinom(k,ratios)
+print(polynom2)
+
+with open('33_Polynomial2.txt', 'w') as data:
+    data.write(polynom2)
