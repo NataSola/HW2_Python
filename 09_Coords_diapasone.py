41af26ce66e5a72da794fd7eac412737d1d0b0b3
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-02-08 00:04:01 +0300

    code execution

--- 09_Coords_diapasone.py
+++ 09_Coords_diapasone.py
@@ -7,6 +7,6 @@ quarter = randint(1, 4)
 diapasones = ['(x > 0, y > 0)', '(x < 0, y > 0)', '(x < 0, y < 0)', '(x > 0, y < 0)']
 
 def show_diapasone (quart, diapasones):
-    return diapasones[quarter-1]
+    return diapasones[quart-1]
 
 print (f'{quarter}: {diapasones[quarter-1]}')
