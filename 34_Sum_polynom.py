e4a8dbbdbf6a2bdb20bca157b11ab94a48deb738
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-02-10 00:36:53 +0300

    Task 34 is solved

--- 34_Sum_polynom.py
+++ 34_Sum_polynom.py
@@ -1,16 +1,22 @@
+# 34. Даны два файла в каждом из которых находится запись многочлена. 
+# Сформировать файл, содержащий сумму многочленов
 
 import re
+import itertools
+
 
 file1 = '33_Polynomial.txt'
 file2 = '33_Polynomial2.txt'
 
 # Получение данных из файла
+
 def read_pol(file):
     with open(str(file), 'r') as data:
         pol = data.read()
     return pol
 
-# Получение списка кортежей (<коэффициент>, <степень>)
+# Получение списка кортежей каждого (<коэффициент>, <степень>)
+
 def convert_pol(pol):
     pol = pol.replace('= 0', '')
     pol = re.sub("[*|^| ]", " ", pol).split('+')
@@ -26,19 +32,34 @@ def convert_pol(pol):
     pol = [tuple(int(x) for x in j if x != 'x') for j in pol]
     return pol
 
-# Получение списка кортежей для суммы полиномов
-# (<коэф1 + коэф2>, <степень>)
-def sum_pols(pol1, pol2):    
-    x = [0] * (max(len(pol1), len(pol2) + 1))
+# Получение списка кортежей суммы (<коэф1 + коэф2>, <степень>)
+
+def fold_pols(pol1, pol2):   
+    x = [0] * (max(pol1[0][1], pol2[0][1] + 1))
     for i in pol1 + pol2:        
         x[i[1]] += i[0]
     res = [(x[i], i) for i in range(len(x)) if x[i] != 0]
     res.sort(key = lambda r: r[1], reverse = True)
     return res
 
+# Составление итогового многочлена
 
-
-# def get_new_pol(pol):
+def get_sum_pol(pol):
+    var = ['*x^'] * len(pol)
+    coefs = [x[0] for x in pol]
+    degrees = [x[1] for x in pol]
+    new_pol = [[str(a), str(b), str(c)] for a, b, c in (zip(coefs, var, degrees))]
+    for x in new_pol:
+        if x[0] == '0': del (x[0])
+        if x[-1] == '0': del (x[-1], x[-1])
+        if len(x) > 1 and x[0] == '1' and x[1] == '*x^': del (x[0], x[0][0])
+        if len(x) > 1 and x[-1] == '1': 
+            del x[-1]
+            x[-1] = '*x'
+        x.append(' + ')
+    new_pol = list(itertools.chain(*new_pol))
+    new_pol[-1] = ' = 0'
+    return "".join(map(str, new_pol))
 
 pol1 = read_pol(file1)
 pol2 = read_pol(file2)
@@ -47,8 +68,6 @@ print(pol2)
 
 pol_1 = convert_pol(pol1)
 pol_2 = convert_pol(pol2)
-print(pol_1)
-print(pol_2)
 
-pol_sum = sum_pols(pol_1, pol_2)
-print(sum_pols(pol_1, pol_2))
+pol_sum = get_sum_pol(fold_pols(pol_1, pol_2))
+print(pol_sum)
